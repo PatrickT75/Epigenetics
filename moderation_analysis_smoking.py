a43a4a6be73f63d68d3b4fcaa3ea454ad7388cae
PatrickT75 <124742404+PatrickT75@users.noreply.github.com>
2023-03-24 10:40:06 -0400

    Update moderation_analysis_smoking.py

--- moderation_analysis_smoking.py
+++ moderation_analysis_smoking.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/13eY5OsLkxC1ONWwYXKnC9aE9gcjKnY_i
 """
 
+#### Generate plots on matplotlib to show moderation and interaction between smoking and predicted age in CIVIC sub-group
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,8 +18,6 @@ import statsmodels.api as sm
 
 file = pd.read_csv("MetadataHuman2021_554.csv")
 
-file
-
 civic = file[224:286]
 
 civic_ages = civic['age'].to_numpy()
@@ -26,9 +26,6 @@ civic_ages_smoker = civic_ages * civic_smoker
 
 civic_stack = np.stack((civic_ages, civic_smoker, civic_ages_smoker), axis = 1)
 civic_stack_df = pd.DataFrame(stack, columns = ['Age', 'Smoker', 'Age*Smoker'])
-
-print(civic_stack_df)
-print(civic_ages)
 
 civic_smoker_model = sm.OLS(civic_ages, civic_stack_df)
 civic_smoker_fit = civic_smoker_model.fit()
@@ -40,15 +37,8 @@ saliva_ages = saliva['age'].to_numpy()
 saliva_smoker = saliva['smoker'].to_numpy()
 saliva_ages_smoker = saliva_ages * saliva_smoker
 
-print(len(saliva_smoker))
-print(len(saliva_ages))
-print(len(saliva_ages_smoker))
-
 saliva_stack = np.stack((saliva_ages, saliva_smoker, saliva_ages_smoker), axis = 1)
 saliva_stack_df = pd.DataFrame(saliva_stack, columns = ['Age', 'Smoker', 'Age*Smoker'])
-
-print(saliva_stack_df)
-print(len(saliva_ages))
 
 saliva_smoker_model = sm.OLS(saliva_ages, saliva_stack_df)
 saliva_smoker_fit = saliva_smoker_model.fit()
@@ -85,8 +75,3 @@ axsmok.legend()
 
 axsmok.bar_label(smok1, labels = smokerstars)
 axsmok.bar_label(smok2, labels = smokermodstars)
-
-# figbuc.savefig('Buccal Moderators.png', dpi=300)
-
-
-
